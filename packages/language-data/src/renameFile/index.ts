eb5c5c28e63fde9b39ecf8ef2c9f3499e1a248f2
Harsh Raj <harshraj8843@gmail.com>
2023-05-19 22:28:26 +0530

    Add @codinasion/language-data

---
+++ packages/language-data/src/renameFile/index.ts
@@ -0,0 +1,55 @@
+import LanguageData from "../data";
+import type { RenameFileInputType } from "../type";
+
+import {
+  FormatStringToCamelCase,
+  FormatStringToKebabCase,
+  FormatStringToPascalCase,
+  FormatStringToSnakeCase,
+} from "@codinasion/tools";
+
+export default function renameFile({
+  name,
+  fileExtension,
+  fileName = "file-name",
+}: RenameFileInputType): string {
+  const language = LanguageData.filter((languageData) => {
+    if (name) {
+      return languageData.name.toLowerCase() === name.toLowerCase();
+    } else if (fileExtension) {
+      let tempFileExtension = "";
+      if (fileExtension[0] === ".") {
+        tempFileExtension = fileExtension.slice(1);
+      }
+      return languageData.fileExtension.includes(tempFileExtension);
+    } else if (fileName) {
+      const tempFileExtension = fileName.split(".").pop();
+      if (tempFileExtension) {
+        return languageData.fileExtension.includes(tempFileExtension);
+      }
+    }
+  });
+
+  const namingConvention = language[0].namingConvention;
+  let fileSlug = fileName.split(".")[0];
+  const fileExt = fileName.split(".").pop();
+
+  switch (namingConvention) {
+    case "PascalCase":
+      fileSlug = FormatStringToPascalCase(fileSlug);
+      break;
+    case "camelCase":
+      fileSlug = FormatStringToCamelCase(fileSlug);
+      break;
+    case "snake_case":
+      fileSlug = FormatStringToSnakeCase(fileSlug);
+      break;
+    case "kebab-case":
+      fileSlug = FormatStringToKebabCase(fileSlug);
+      break;
+    default:
+      break;
+  }
+
+  return `${fileSlug}.${fileExt}`;
+}
